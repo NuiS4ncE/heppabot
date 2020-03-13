b9a9bfc4e9521f4cd3645ef56f8399a020544e2c
NuiS4ncE <nuis4nce@hotmail.com>
2020-03-13 21:26:07 +0200

    amount of movies according to top+number set

--- src/main/java/tkoaly/pohinatiimi/laudekoodit/LaudeBot.java
+++ src/main/java/tkoaly/pohinatiimi/laudekoodit/LaudeBot.java
@@ -13,28 +13,39 @@ import org.jsoup.select.Elements;
 import java.util.ArrayList;
 
 public class LaudeBot extends TelegramLongPollingBot {
-    
+
     private static final String heppaUrl = "https://heppa.herokuapp.com/candidates";
     private Dotenv dotenv = Dotenv.load();
+    private int num, moviesize;
+    
 
     @Override
     public void onUpdateReceived(Update update) {
-        
-        
+
         Long chatId = update.getMessage().getChatId();
         String text = update.getMessage().getText();
-        
+
         // Initialize answer message
         SendMessage answer = new SendMessage()
-            .setChatId(chatId);
+                .setChatId(chatId);
         
+        Document doc = Jsoup.parse(fetchHeppa());
+        Element table = doc.select("table").get(0);
+        Elements movies = table.select("tr");
+        moviesize = movies.size() - 1;
         // Choose text for answer
-        if (text.equals("Hello")) {
+        int inputnum = Integer.parseInt(text.substring(4));
+        if (inputnum > moviesize) {
+            answer.setText("These are not the answers you're looking for. Amount of movies is " + moviesize);
+        }
+        else if (text.substring(0, 4).equals("/top")) {
+
+            num = inputnum;
             answer.setText(buildMessage());
         } else {
-            answer.setText("Happy hacking!");
+            answer.setText("Whatchu talking about, foo!");
         }
-        
+
         // Send message
         try {
             execute(answer);
@@ -52,21 +63,20 @@ public class LaudeBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return dotenv.get("BOT_TOKEN");
     }
-    
-    
+
     private String fetchHeppa() {
         String heppaHtml = "";
-        
+
         try {
             heppaHtml = Jsoup.connect(heppaUrl).get().html();
         } catch (Exception e) {
-            
+
         }
 
         return heppaHtml;
     }
-    
-    private String findMovie(){
+
+    /* private String findMovie(){
         try{
         String url = "https://heppa.herokuapp.com/candidates";
         Document document = Jsoup.connect(url).get();
@@ -83,8 +93,7 @@ public class LaudeBot extends TelegramLongPollingBot {
             System.out.println(e.toString());
             return e.toString().substring(0,100);
         }
-    }
-    
+    } */
     private ArrayList<String> parseTopMovies(String heppaHtml) {
         Document doc = Jsoup.parse(heppaHtml);
         Element table = doc.select("table").get(0);
@@ -92,11 +101,11 @@ public class LaudeBot extends TelegramLongPollingBot {
         
         ArrayList<String> topMovies = new ArrayList<>();
 
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < Math.min(num + 1, movies.size()); i++) {
             Elements fields = movies.get(i).select("td");
-            
+
             String movieEntry = "";
-            
+
             movieEntry += fields.get(0).text();
             movieEntry += " - ";
             movieEntry += fields.get(3).text();
@@ -111,7 +120,7 @@ public class LaudeBot extends TelegramLongPollingBot {
     private String buildMessage() {
         String msg = "";
         ArrayList<String> movies = parseTopMovies(fetchHeppa());
-        
+
         for (int i = 0; i < movies.size(); i++) {
             msg += movies.get(i);
             msg += "\n";
@@ -119,5 +128,5 @@ public class LaudeBot extends TelegramLongPollingBot {
 
         return msg;
     }
-    
+
 }
